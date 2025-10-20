6505dff46b1d7836d66fc43bea9ddd718452f8ce
Farzin <farzinahmadzade909@gmail.com>
2025-10-20 21:18:36 +0330

    Create model.py

---
+++ model.py
@@ -0,0 +1,54 @@
+import segmentation_models_pytorch as smp
+import torch
+import torch.nn as nn
+
+class SegmentationModel:
+    def __init__(self, model_name='unet', encoder_name='resnet34', num_classes=5, encoder_weights='imagenet'):
+        self.model_name = model_name
+        self.num_classes = num_classes
+        
+        if model_name == 'unet':
+            self.model = smp.Unet(
+                encoder_name=encoder_name,
+                encoder_weights=encoder_weights,
+                classes=num_classes,
+                activation=None
+            )
+        elif model_name == 'deeplabv3plus':
+            self.model = smp.DeepLabV3Plus(
+                encoder_name=encoder_name,
+                encoder_weights=encoder_weights,
+                classes=num_classes,
+                activation=None
+            )
+        elif model_name == 'pspnet':
+            self.model = smp.PSPNet(
+                encoder_name=encoder_name,
+                encoder_weights=encoder_weights,
+                classes=num_classes,
+                activation=None
+            )
+        elif model_name == 'fpn':
+            self.model = smp.FPN(
+                encoder_name=encoder_name,
+                encoder_weights=encoder_weights,
+                classes=num_classes,
+                activation=None
+            )
+        else:
+            raise ValueError(f"Model {model_name} not supported")
+    
+    def to_device(self, device):
+        self.model = self.model.to(device)
+        return self
+    
+    def get_model(self):
+        return self.model
+
+def get_available_models():
+    """لیست مدل‌های قابل استفاده"""
+    return ['unet', 'deeplabv3plus', 'pspnet', 'fpn']
+
+def get_available_encoders():
+    """لیست encoderهای قابل استفاده"""
+    return ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'efficientnet-b0', 'efficientnet-b1']
