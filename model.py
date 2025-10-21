eff2c0a4b73f9c9884ab013b3e80620493c059de
Farzin <farzinahmadzade909@gmail.com>
2025-10-21 11:33:15 +0330

    Update model.py

--- model.py
+++ model.py
@@ -1,34 +1,79 @@
+import torch
+import torch.nn as nn
 import segmentation_models_pytorch as smp
+from data_loader import Config
 
-class SegmentationModel:
-    def __init__(self, model_name='unet', encoder_name='resnet34', num_classes=5, encoder_weights='imagenet'):
-        self.model_name = model_name
-        self.num_classes = num_classes
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+class ManualUNet(nn.Module):
+    def __init__(self, n_classes=Config.NUM_CLASSES):
+        super().__init__()
+        self.enc1 = DoubleConv(3, 64)
+        self.enc2 = DoubleConv(64, 128)
+        self.enc3 = DoubleConv(128, 256)
+        self.enc4 = DoubleConv(256, 512)
+        self.bottleneck = DoubleConv(512, 1024)
+        self.up4 = nn.ConvTranspose2d(1024, 512, 2, stride=2)
+        self.dec4 = DoubleConv(1024, 512)
+        self.up3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+        self.up2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+        self.up1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+        self.final = nn.Conv2d(64, n_classes, 1)
         
-        if model_name == 'unet':
-            self.model = smp.Unet(encoder_name=encoder_name, encoder_weights=encoder_weights,
-                                  classes=num_classes, activation=None)
-        elif model_name == 'deeplabv3plus':
-            self.model = smp.DeepLabV3Plus(encoder_name=encoder_name, encoder_weights=encoder_weights,
-                                           classes=num_classes, activation=None)
-        elif model_name == 'pspnet':
-            self.model = smp.PSPNet(encoder_name=encoder_name, encoder_weights=encoder_weights,
-                                    classes=num_classes, activation=None)
-        elif model_name == 'fpn':
-            self.model = smp.FPN(encoder_name=encoder_name, encoder_weights=encoder_weights,
-                                 classes=num_classes, activation=None)
-        else:
-            raise ValueError(f"Model {model_name} not supported")
-    
-    def to_device(self, device):
-        self.model = self.model.to(device)
-        return self
-    
-    def get_model(self):
-        return self.model
+    def forward(self, x):
+        e1 = self.enc1(x)
+        e2 = self.enc2(nn.functional.max_pool2d(e1, 2))
+        e3 = self.enc3(nn.functional.max_pool2d(e2, 2))
+        e4 = self.enc4(nn.functional.max_pool2d(e3, 2))
+        b = self.bottleneck(nn.functional.max_pool2d(e4, 2))
+        
+        d4 = self.up4(b)
+        d4 = torch.cat([d4, e4], dim=1)
+        d4 = self.dec4(d4)
+        d3 = self.up3(d4)
+        d3 = torch.cat([d3, e3], dim=1)
+        d3 = self.dec3(d3)
+        d2 = self.up2(d3)
+        d2 = torch.cat([d2, e2], dim=1)
+        d2 = self.dec2(d2)
+        d1 = self.up1(d2)
+        d1 = torch.cat([d1, e1], dim=1)
+        d1 = self.dec1(d1)
+        
+        return self.final(d1)
 
-def get_available_models():
-    return ['unet', 'deeplabv3plus', 'pspnet', 'fpn']
+class SMPUNet(nn.Module):
+    def __init__(self, encoder_name="resnet34", n_classes=Config.NUM_CLASSES):
+        super().__init__()
+        self.model = smp.Unet(
+            encoder_name=encoder_name,
+            encoder_weights="imagenet",
+            in_channels=3,
+            classes=n_classes
+        )
 
-def get_available_encoders():
-    return ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'efficientnet-b0', 'efficientnet-b1']
+    def forward(self, x):
+        return self.model(x)
+
+def get_model(model_name="manual", encoder_name="resnet34"):
+    if model_name.lower() == "manual":
+        return ManualUNet()
+    elif model_name.lower() == "smp":
+        return SMPUNet(encoder_name=encoder_name)
+    raise ValueError("model_name must be 'manual' or 'smp'")
