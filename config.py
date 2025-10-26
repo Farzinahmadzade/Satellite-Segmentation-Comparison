35f2180baaf69a6e4866e72bcb5040a59075c92b
Farzin <farzinahmadzade909@gmail.com>
2025-10-26 10:17:41 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+    IMAGE_SIZE = (256, 256)
+    NUM_CLASSES = 9
+    CLASS_NAMES = [
+        "bareland", "rangeland", "development", "road", "tree",
+        "water", "agricultural", "building", "nodata" ]
+    BATCH_SIZE = 4
+    EPOCHS = 5
+    LEARNING_RATE = 1e-4
+    VAL_SPLIT = 0.2
+    NUM_WORKERS = 2
