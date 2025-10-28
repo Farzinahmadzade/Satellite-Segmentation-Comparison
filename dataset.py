04287278564f7b8d0176b17c5a7a96f2d089d2ce
Farzin <farzinahmadzade909@gmail.com>
2025-10-28 10:36:07 +0330

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,12 +1,13 @@
 import os
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
 from PIL import Image
 import numpy as np
-from config import Config
 
 class SegmentationDataset(Dataset):
+
     def __init__(self, data_dir, split="train", image_size=256, val_split=0.2, limit_samples=0):
         self.images = []
         self.masks = []
@@ -16,16 +17,14 @@ class SegmentationDataset(Dataset):
             if "images" in dirs and "labels" in dirs:
                 img_dir = os.path.join(root, "images")
                 lbl_dir = os.path.join(root, "labels")
-
                 imgs = sorted([os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.lower().endswith(".tif")])
                 lbls = sorted([os.path.join(lbl_dir, f) for f in os.listdir(lbl_dir) if f.lower().endswith(".tif")])
-
                 img_dict = {os.path.splitext(os.path.basename(p))[0].lower(): p for p in imgs}
                 lbl_dict = {os.path.splitext(os.path.basename(p))[0].lower(): p for p in lbls}
                 common = sorted(set(img_dict.keys()) & set(lbl_dict.keys()))
 
                 if len(common) < len(imgs) or len(common) < len(lbls):
-                    skipped = (len(imgs) + len(lbls)) - 2*len(common)
+                    skipped = (len(imgs) + len(lbls)) - 2 * len(common)
                     print(f"⚠️ Skipping {skipped} unmatched files in {os.path.basename(root)}")
 
                 self.images.extend([img_dict[k] for k in common])
@@ -49,20 +48,28 @@ class SegmentationDataset(Dataset):
 
         print(f"Dataset ({split}): {len(self.images)} samples")
 
-        self.transform_img = transforms.Compose([
-            transforms.Resize(self.image_size),
-            transforms.ToTensor(),])
-        
-        self.transform_mask = transforms.Compose([
-            transforms.Resize(self.image_size, interpolation=Image.NEAREST),])
+        if isinstance(self.image_size, int):
+            height = width = self.image_size
+        else:
+            height, width = self.image_size
+
+        self.transform = A.Compose([
+            A.Resize(height=height, width=width),
+            A.HorizontalFlip(p=0.5),
+            A.RandomRotate90(p=0.5),
+            A.Normalize(mean=(0.485, 0.456, 0.406),
+                        std=(0.229, 0.224, 0.225)),
+            ToTensorV2() ])
 
     def __len__(self):
         return len(self.images)
 
+
     def __getitem__(self, idx):
-        img = Image.open(self.images[idx]).convert("RGB")
-        mask = Image.open(self.masks[idx])
-        mask = np.array(self.transform_mask(mask), dtype=np.int64)
-        mask = torch.from_numpy(mask).long()
-        img = self.transform_img(img)
-        return img, mask
+        img = np.array(Image.open(self.images[idx]).convert("RGB"))
+        mask = np.array(Image.open(self.masks[idx]), dtype=np.int64)
+        transformed = self.transform(image=img, mask=mask)
+        image_tensor = transformed["image"].float()
+        mask_tensor = transformed["mask"].long()
+
+        return image_tensor, mask_tensor
