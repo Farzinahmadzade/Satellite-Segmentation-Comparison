13cf1e3da99e9c8a10d1d8dadce619c0ce0feed3
Farzin <farzinahmadzade909@gmail.com>
2025-10-28 10:37:15 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,16 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-from torchvision.utils import make_grid
 
 class Metrics:
     @staticmethod
     def compute_batch_metrics(outputs, masks, num_classes):
+
         preds = torch.argmax(outputs, dim=1)
-        intersection = torch.zeros(num_classes)
-        union = torch.zeros(num_classes)
+        device = preds.device
+
+        intersection = torch.zeros(num_classes, device=device)
+        union = torch.zeros(num_classes, device=device)
         correct = (preds == masks).sum().item()
         total = masks.numel()
 
@@ -25,9 +27,19 @@ class Metrics:
 
     @staticmethod
     def visualize_sample(image, mask, output, idx, writer, epoch):
+
         image = image.detach().cpu()
-        mask = mask.detach().cpu()
-        pred = torch.argmax(output.detach().cpu(), dim=0)
+        if mask.dim() == 3 and mask.size(0) == 1:
+            mask = mask.squeeze(0).detach().cpu()
+        else:
+            mask = mask.detach().cpu()
+
+        if output.dim() == 4:
+            out = output.detach().cpu().squeeze(0)
+        else:
+            out = output.detach().cpu()
+
+        pred = torch.argmax(out, dim=0)
 
         fig, axes = plt.subplots(1, 3, figsize=(9, 3))
         axes[0].imshow(image.permute(1, 2, 0))
