a072762c315ba9387b9e1133463b2c65aa441ade
Farzin <farzinahmadzade909@gmail.com>
2025-10-29 12:23:28 +0330

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import torch
-import numpy as np
 import matplotlib.pyplot as plt
+
 
 class Metrics:
     @staticmethod
-    def compute_batch_metrics(outputs, masks, num_classes):
-
+    def compute_batch_metrics(outputs: torch.Tensor, masks: torch.Tensor, num_classes: int):
+        """Compute IoU, Dice, and Accuracy for a batch."""
         preds = torch.argmax(outputs, dim=1)
         device = preds.device
 
@@ -27,26 +27,18 @@ class Metrics:
 
     @staticmethod
     def visualize_sample(image, mask, output, idx, writer, epoch):
-
+        """Visualize input, mask, and prediction in TensorBoard."""
         image = image.detach().cpu()
-        if mask.dim() == 3 and mask.size(0) == 1:
-            mask = mask.squeeze(0).detach().cpu()
-        else:
-            mask = mask.detach().cpu()
-
-        if output.dim() == 4:
-            out = output.detach().cpu().squeeze(0)
-        else:
-            out = output.detach().cpu()
-
+        mask = mask.squeeze(0).detach().cpu() if mask.dim() == 4 else mask.detach().cpu()
+        out = output.squeeze(0).detach().cpu() if output.dim() == 4 else output.detach().cpu()
         pred = torch.argmax(out, dim=0)
 
         fig, axes = plt.subplots(1, 3, figsize=(9, 3))
         axes[0].imshow(image.permute(1, 2, 0))
         axes[0].set_title("Image")
-        axes[1].imshow(mask)
+        axes[1].imshow(mask, cmap='tab20', vmin=0, vmax=8)
         axes[1].set_title("Mask")
-        axes[2].imshow(pred)
+        axes[2].imshow(pred, cmap='tab20', vmin=0, vmax=8)
         axes[2].set_title("Prediction")
         for ax in axes:
             ax.axis("off")
