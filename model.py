af3b7ce4fd71cd5ad67532dd909ac3182110942a
Farzin <farzinahmadzade909@gmail.com>
2025-10-29 12:20:04 +0330

    Update model.py

--- model.py
+++ model.py
@@ -1,31 +1,24 @@
 import segmentation_models_pytorch as smp
 
-def get_model(model_name='unet', encoder_name='resnet34', in_channels=3, classes=9):
+
+def get_model(model_name: str = 'unet', encoder_name: str = 'resnet34', in_channels: int = 3, classes: int = 9):
     name = model_name.lower()
 
-    if name == 'unet':
-        model = smp.Unet(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name in ['unet++', 'unetplusplus', 'unetplus']:
-        model = smp.UnetPlusPlus(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'fpn':
-        model = smp.FPN(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'pspnet':
-        model = smp.PSPNet(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'deeplabv3':
-        model = smp.DeepLabV3(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name in ['deeplabv3+', 'deeplabv3plus']:
-        model = smp.DeepLabV3Plus(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'linknet':
-        model = smp.Linknet(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'manet':
-        model = smp.MAnet(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'pan':
-        model = smp.PAN(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'upernet':
-        model = smp.UPerNet(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    elif name == 'segformer':
-        model = smp.Segformer(encoder_name=encoder_name, in_channels=in_channels, classes=classes)
-    else:
+    models = {
+        'unet': smp.Unet,
+        'unet++': smp.UnetPlusPlus,
+        'fpn': smp.FPN,
+        'pspnet': smp.PSPNet,
+        'deeplabv3': smp.DeepLabV3,
+        'deeplabv3+': smp.DeepLabV3Plus,
+        'linknet': smp.Linknet,
+        'manet': smp.MAnet,
+        'pan': smp.PAN,
+        'upernet': smp.UPerNet,
+        'segformer': smp.Segformer,
+    }
+
+    if name not in models:
         raise NotImplementedError(f"Model '{model_name}' not supported.")
 
-    return model
+    return models[name](encoder_name=encoder_name, in_channels=in_channels, classes=classes)
